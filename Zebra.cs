0f7b68f9beaf46dbe4ba3069d9c8215f89430009
Yakov <medvejonok07@gmail.com>
2020-01-08 10:59:46 +0800

    Zebra.cs

---
+++ Zebra.cs
@@ -0,0 +1,8 @@
+using System;
+class Zebra
+{
+	public Zebra
+	{
+		Console.WriteLine("Marti");
+	}
+}
--- Zoo.cs
+++ Zoo.cs
@@ -4,6 +4,7 @@ class Zoo
 	public static void Main()
 	{
 		Console.WriteLine("Welcome Zoo!");
+		Zebra zebra = new Zebra();
 	}
 	
 }
